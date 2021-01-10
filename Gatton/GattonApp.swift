c8e58b0cc9b656c32c1404b708c64a7088023967
Andrew Park <andysampark@gmail.com>
2021-01-10 12:52:24 -0500

    Initial Commit

---
+++ Gatton/GattonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GattonApp.swift
+//  Gatton
+//
+//  Created by Andrew Park on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GattonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
